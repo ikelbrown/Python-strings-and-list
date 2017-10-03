99b7263c613dd719734eda05f2c817d581bc91c8
ikebrown <ikesmarketing@icloud.com>
2017-10-03 15:14:19 -0700

    learning to create loops and iterate through list and strings and such...I love being a newbee

---
+++ filtertype.py
@@ -0,0 +1,42 @@
+#Filter By Type
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers"
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1,7,4,21]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+
+
+#Interger
+count = 120
+if count >= 100:
+    print "That's a big number!"
+    count = count - 1
+else:
+        print "That's a small number"
+
+
+#String
+x = "carry my bag please"
+count = 0
+for  i in x:
+    if count >=50:
+        print "Long sentence."
+    elif count <50:
+        print "Short sentence"
+
+#List
+ikes_list = [1,2,3,4,5,-6,7,8,9,10,0]
+if len(ikes_list) >= 10:
+    print "Big list!"
+else:
+    print "Short list"
+    
