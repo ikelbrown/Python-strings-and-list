808e7441941ccb2d1b2cf3d8769040f86be043a3
ikebrown <ikesmarketing@icloud.com>
2017-10-03 12:21:16 -0700

    Python assignment on math Multiples Sum and Average

---
+++ pyfun1.py
@@ -0,0 +1,46 @@
+# Multiples
+count = 1
+while count < 10001:
+    print count
+    count +=2
+
+count = 5
+while count < 1000001:
+    print count
+    count+=5
+'''#multiples A
+for count in range(1, 1001, 2):
+    print count
+
+#multiples B
+for count in range(5,1000001,5):
+    print count'''
+
+#Sum List
+a = [1, 2, 5, 10, 255, 3]
+count = 0
+for i in a:
+    count += i
+print count
+    
+'''#sum list
+my_numbers = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in my_numbers:
+    sum += i
+print sum'''
+
+
+ #Average List
+print count/len(a)
+
+'''a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for element in a:
+    sum += [a]
+    avg = sum(a)/len(a)
+    print avg
+
+
+#average list
+print sum/len(my_numbers)'''
